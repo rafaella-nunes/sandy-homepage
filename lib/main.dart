5ea522cf62b9f21c3f1767d302a61c201bc59726
rafaella-nunes <faellanunes01@gmail.com>
2024-05-03 18:19:26 -0300

    chore: fix image, final commit

--- lib/main.dart
+++ lib/main.dart
@@ -38,86 +38,91 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: const Color(0xFFFAFBFC),
       body: Stack(
-        children: [
-          ClipPath(
-            clipper: CurveClipper(),
-            child: Container(
-              color: const Color(0xFFF1F1F1),
-              height: MediaQuery.of(context).size.height * 0.5,
+        fit: StackFit.expand,
+        children:[ Stack(
+          children: [
+            ClipPath(
+              clipper: CurveClipper(),
+              child: Container(
+                color: const Color(0xFFF1F1F1),
+                height: MediaQuery.of(context).size.height * 0.56,
+              ),
             ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RichText(
-                  text: const TextSpan(
-                    text: 'Olá, eu sou a',
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RichText(
+                    text: const TextSpan(
+                      text: 'Olá, eu sou a',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 23,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF383838)),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: ' Sandy 👋',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF8342FF),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    'Estou aqui para te ajudar',
                     style: TextStyle(
                         fontFamily: 'Montserrat',
-                        fontSize: 25,
-                        fontWeight: FontWeight.w700,
-                        color: Color(0xFF383838)),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: ' Sandy 👋',
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF8342FF),
-                        ),
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 116, 116, 116)),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Image.asset(
+                    'assets/images/sandy.png',
+                    height: 450,
+                    ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Ação a ser executada quando o botão for pressionado
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF8342FF), // Cor de fundo do botão
+                      foregroundColor: Colors.white, // Cor do texto do botão
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 18), // Espaçamento interno do botão
+                      textStyle: const TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  'Estou aqui para te ajudar',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 116, 116, 116)),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Image.asset(
-                  'assets/images/sandy.png',
-                  ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Ação a ser executada quando o botão for pressionado
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        const Color(0xFF8342FF), // Cor de fundo do botão
-                    foregroundColor: Colors.white, // Cor do texto do botão
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30,
-                        vertical: 18), // Espaçamento interno do botão
-                    textStyle: const TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            30), // Borda arredondada do botão
+                      ),
                     ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          30), // Borda arredondada do botão
-                    ),
+                    child: const Text('CONVERSAR COM A SANDY'),
                   ),
-                  child: const Text('CONVERSAR COM A SANDY'),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+          ],
+        ),
         ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
